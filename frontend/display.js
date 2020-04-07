dc612f129ef8d4c84553b74a12c07a405093047a
Leo Lee <41759796+smjleo@users.noreply.github.com>
2020-04-07 16:27:58 +0800

    Update display.js

--- frontend/display.js
+++ frontend/display.js
@@ -8,8 +8,8 @@ function displayPixel() {
     setInterval(() => {
         fetch('http://127.0.0.1:5000/render?r=' + row + '&c=' + column)
             .then(res => res.text())
-            .then(res => {
-                switch (res) {
+            .then(body => {
+                switch (body) {
                     case '0':
                         favicon.setAttribute('href', './assets/grey.ico');
                         break;
